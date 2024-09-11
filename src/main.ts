16c16bb2d68409eab958d29503a4f14290e4f586
JohnnyCobzari <105036285+JohnnyCobzari@users.noreply.github.com>
2024-09-11 08:37:30 +0300

    backend on port 3002 allows requests

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // If you want to include cookies in CORS requests
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
