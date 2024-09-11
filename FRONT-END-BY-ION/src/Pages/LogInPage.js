20e257ff174c4122191187cb27eac590756aba26
JohnnyCobzari <105036285+JohnnyCobzari@users.noreply.github.com>
2024-09-11 12:37:11 +0300

    git push

--- FRONT-END-BY-ION/src/Pages/LogInPage.js
+++ FRONT-END-BY-ION/src/Pages/LogInPage.js
@@ -5,6 +5,22 @@ import Logo from "../components/Logo";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: 'http://localhost:3002', // Your backend URL
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+  }
+});
+
+// Function to set the token in headers
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers['Authorization'];
+  }
+};
 
 function LoginPage() {
   
