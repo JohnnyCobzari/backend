851c3014162319da1e3878c6fd91a0ea15b8f9a8
JohnnyCobzari <105036285+JohnnyCobzari@users.noreply.github.com>
2024-09-24 13:36:50 +0300

    added match functionality

--- src/pet/pet.module.ts
+++ src/pet/pet.module.ts
@@ -4,11 +4,13 @@ import { PetController } from './pet.controller';
 import { PetService } from './pet.service';
 import { PetSchema } from './schemas/pet.schema';
 import { AuthModule } from 'src/auth/auth.module';
+import { MatchSchema } from './schemas/match.schema';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forFeature([{name: 'Pet', schema: PetSchema}])],
+    MongooseModule.forFeature([{name: 'Pet', schema: PetSchema}]),
+    MongooseModule.forFeature([{name: 'Match', schema: MatchSchema}])],
   controllers: [PetController],
   providers: [PetService]
 })
